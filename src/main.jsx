32dcf35ff6ed84ca3c1e264c26fe66275534cfca
ernest-jere <ernestmmjere@outlook.com>
2026-09-01 10:53:51 +0200

    Initialize GA4 with your Vite Environment Variable

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ReactGA from 'react-ga4'
+
+// Initialize GA4 with your Vite Environment Variable
+const MEASUREMENT_ID = import.meta.env.VITE_GA_MEASUREMENT_ID;
+
+if (MEASUREMENT_ID) {
+  ReactGA.initialize(MEASUREMENT_ID);
+  
+  // Send the initial pageview
+  ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
